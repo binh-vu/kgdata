2e23e22c76bb2b1af08df0294739040a4a47276d
Binh Vu <binh@toan2.com>
2021-05-28 12:53:15 -0700

    update

--- kgdata/wikidata/db.py
+++ kgdata/wikidata/db.py
@@ -52,7 +52,19 @@ class WDProxyDB(RocksDBStore[str, V]):
         self.db.put(key.encode(), value.serialize())
 
     def __contains__(self, key):
-        return self.db.get(key.encode()) != b'\x00'
+        item = self.db.get(key.encode())
+        if item == b'\x00':
+            return False
+        if item is None:
+            qnodes = query_wikidata_entities([key])
+            if len(qnodes) == 0:
+                # no entity
+                self.db.put(key.encode(), b'\x00')
+                return False
+
+            ent = self.extract_ent_from_qnode(qnodes[key])
+            self.db.put(key.encode(), ent.serialize())
+        return True
 
     def get(self, key: str, default=None):
         item = self.db.get(key.encode())
@@ -93,6 +105,7 @@ def get_wdprop_db(dbfile: str, create_if_missing=True, read_only=False, proxy: b
 
 
 def query_wikidata_entities(qnode_ids: Union[Set[str], List[str]]) -> Dict[str, QNode]:
+    assert len(qnode_ids) > 0, qnode_ids
     resp = requests.get("https://www.wikidata.org/w/api.php", params={
         "action": "wbgetentities",
         "ids": "|".join(qnode_ids),
@@ -100,7 +113,15 @@ def query_wikidata_entities(qnode_ids: Union[Set[str], List[str]]) -> Dict[str, 
     })
     assert resp.status_code, resp
     data = resp.json()
-    assert data['success'] == 1
+    if 'success' not in data:
+        assert 'error' in data, data
+        # we have invalid entity id format, if we only query for one entity
+        # we can tell it doesn't exist, otherwise, we don't know which entity are wrong
+        if len(qnode_ids) == 1:
+            return {}
+        raise Exception(f"Invalid entity ID format. Don't know which entities are wrong. {qnode_ids}")
+    else:
+        assert data.get('success', None) == 1, data
     qnodes = {}
 
     for qnode_id in qnode_ids:
