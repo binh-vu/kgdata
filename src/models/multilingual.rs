9c029b75be4953c631e53a3a9bdff3904fded2ad
Binh Vu <binh@toan2.com>
2023-02-18 15:14:03 -0800

    util func to get default text in rust model

--- src/models/multilingual.rs
+++ src/models/multilingual.rs
@@ -7,8 +7,20 @@ pub struct MultiLingualString {
     pub lang2value: HashMap<String, String>,
 }
 
+impl MultiLingualString {
+    pub fn get_default_value(&self) -> &String {
+        self.lang2value.get(&self.lang).unwrap()
+    }
+}
+
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct MultiLingualStringList {
     pub lang: String,
     pub lang2values: HashMap<String, Vec<String>>,
+}
+
+impl MultiLingualStringList {
+    pub fn get_default_values(&self) -> &Vec<String> {
+        self.lang2values.get(&self.lang).unwrap()
+    }
 }
