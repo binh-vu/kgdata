6ac491847ead734cf73d631e8108ff9d4cb208bb
Binh Vu <binh@toan2.com>
2023-08-16 12:52:21 -0700

    fix wikidata.datasets.entity_all_types

--- kgdata/wikidata/datasets/entity_all_types.py
+++ kgdata/wikidata/datasets/entity_all_types.py
@@ -1,58 +1,60 @@
 from __future__ import annotations
 
 import math
-from random import randrange
-from typing import Iterable, Optional
-
-import orjson
+from functools import partial
 
 from kgdata.dataset import Dataset
+from kgdata.dbpedia.datasets.entity_all_types import (
+    EntityAllTypes,
+    extrapolate_class,
+    flip_types,
+    merge_type_dist,
+    merge_types,
+)
 from kgdata.spark import are_records_unique, does_result_dir_exist, get_spark_context
 from kgdata.wikidata.config import WikidataDirCfg
 from kgdata.wikidata.datasets.class_count import class_count
 from kgdata.wikidata.datasets.classes import classes
 from kgdata.wikidata.datasets.entity_types import entity_types
-from kgdata.wikidata.models.wdclass import WDClass
+
+# approximated size of data sent to each worker to ensure even distrubuted workload
+PARTITION_SIZE = 10000
 
 
-def entity_all_types(lang="en") -> Dataset[tuple[str, list[str]]]:
+def entity_all_types(lang="en") -> Dataset[EntityAllTypes]:
     cfg = WikidataDirCfg.get_instance()
 
     unique_check = False
 
     if not does_result_dir_exist(cfg.entity_all_types):
-        # if the number of records of a class exceeds this threshold, we will
-        # partition records of the class to make workloads even for each workers
-        threshold = 10000
-
-        type_and_threshold = (
+        id2count = (
             class_count(lang)
             .get_rdd()
-            .filter(lambda tup: tup[1] > threshold)
-            .map(lambda tup: (tup[0], math.ceil(tup[1] / threshold)))
+            .filter(lambda tup: tup[1] > PARTITION_SIZE)
+            .map(lambda tup: (tup[0], math.ceil(tup[1] / PARTITION_SIZE)))
             .collect()
         )
-        type_and_threshold = {tup[0]: tup[1] for tup in type_and_threshold}
+        id2count = {tup[0]: tup[1] for tup in id2count}
 
         sc = get_spark_context()
-        bc_type_and_threshold = sc.broadcast(type_and_threshold)
+        bc_id2count = sc.broadcast(id2count)
 
-        cls_ancestors = (
+        id2ancestors = (
             classes()
             .get_rdd()
-            .flatMap(lambda c: extrapolate_class(c, bc_type_and_threshold.value))
+            .flatMap(lambda c: extrapolate_class(c, bc_id2count.value))
         )
 
         (
             entity_types(lang)
             .get_rdd()
-            .flatMap(lambda tup: flip_types(tup, bc_type_and_threshold.value))
+            .flatMap(partial(flip_types, type_count=bc_id2count.value))
             .groupByKey()
-            .leftOuterJoin(cls_ancestors)
+            .leftOuterJoin(id2ancestors)
             .flatMap(merge_types)
             .groupByKey()
-            .map(lambda x: (x[0], list(set(x[1]))))
-            .map(orjson.dumps)
+            .map(lambda x: EntityAllTypes(x[0], merge_type_dist(x[1])))
+            .map(EntityAllTypes.ser)
             .saveAsTextFile(
                 str(cfg.entity_all_types),
                 compressionCodecClass="org.apache.hadoop.io.compress.GzipCodec",
@@ -61,40 +63,10 @@ def entity_all_types(lang="en") -> Dataset[tuple[str, list[str]]]:
 
         unique_check = True
 
-    ds = Dataset(file_pattern=cfg.entity_all_types / "*.gz", deserialize=orjson.loads)
+    ds = Dataset(
+        file_pattern=cfg.entity_all_types / "*.gz", deserialize=EntityAllTypes.deser
+    )
     if unique_check:
-        assert are_records_unique(ds.get_rdd(), lambda x: x[0])
+        assert are_records_unique(ds.get_rdd(), lambda x: x.id)
 
     return ds
-
-
-def extrapolate_class(
-    cls: WDClass, type_count: dict[str, int]
-) -> list[tuple[str, list[str]]]:
-    ancestors = list(cls.ancestors)
-    if cls.id in type_count:
-        return [(cls.id + "_" + str(i), ancestors) for i in range(type_count[cls.id])]
-
-    return [(cls.id, ancestors)]
-
-
-def flip_types(tup: tuple[str, list[str]], type_count: dict[str, int]):
-    eid, types = tup
-    out = []
-
-    for t in types:
-        if t not in type_count:
-            out.append((t, eid))
-        else:
-            out.append((t + "_" + str(randrange(type_count[t])), eid))
-    return out
-
-
-def merge_types(tup: tuple[str, tuple[Iterable[str], Optional[list[str]]]]):
-    type_id, (ent_ids, type_ancestors) = tup
-    assert type_ancestors is not None
-
-    new_types = type_ancestors.copy()
-    new_types.append(type_id.split("_", 1)[0])
-
-    return [(ent_id, t) for ent_id in ent_ids for t in new_types]
