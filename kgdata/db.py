11c47a282e4b7908e2c974e0f83586a217e8dc09
Binh Vu <binh@toan2.com>
2023-12-04 21:08:38 -0800

    users can use db without install pyspark as it's an optional dependency

--- kgdata/db.py
+++ kgdata/db.py
@@ -24,7 +24,6 @@ from loguru import logger
 from rdflib import RDF, RDFS, XSD
 
 import serde.json
-from kgdata.dataset import Dataset
 from kgdata.models.entity import Entity
 from kgdata.models.multilingual import MultiLingualString, MultiLingualStringList
 from kgdata.models.ont_class import OntologyClass
@@ -286,6 +285,7 @@ def build_database(
         compact: whether to compact the database
         format: format of the files in the dataset
     """
+    from kgdata.dataset import Dataset
 
     def db_options():
         db = get_db()
